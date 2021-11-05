53991a6dde975b666a30533b9e63a10b20783fcb
Marcos Neves <marcos.neves@gmail.com>
2021-11-05 17:37:32 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // sample file to run on a native machine using systemd:
 // https://blog.r0b.io/post/running-node-js-as-a-systemd-service/
 // deno run -Ar 
-import { listenAndServe } from "https://deno.land/std@0.113.0/http/server.ts"
+import { listenAndServe } from "https://deno.land/std/http/server.ts"
 
 const boot = new Date().toJSON()
 let counter = 0
@@ -9,7 +9,8 @@ let counter = 0
 console.log("Listening on http://localhost:8080")
 
 await listenAndServe(":8080", (_req) => {
-  const body = `counter: ${counter++}. boot: ${boot}`
+  const memory = Object.entries(Deno.memoryUsage()).map(([a, b]) => [a, Math.round(b/1024/1024, 2)])
+  const body = `counter: ${counter++}. boot: ${boot}. Memory: ${memory}`
   
   return new Response(body, {
     headers: { "content-type": "text/plain" },
