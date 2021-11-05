e0183354078274abe19449425c4089e20e8f3520
Marcos Neves <marcos.neves@gmail.com>
2021-11-05 17:43:25 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 // https://blog.r0b.io/post/running-node-js-as-a-systemd-service/
 // deno run -Ar 
 import { listenAndServe } from "https://deno.land/std/http/server.ts"
-
+const VERSION = 1
 const boot = new Date().toJSON()
 let counter = 0
 
@@ -12,7 +12,7 @@ await listenAndServe(":8080", (req) => {
   if (req.url.endsWith("/throw")) throw "error"
   
   const memory = Object.entries(Deno.memoryUsage()).map(([a, b]) => [a, Math.round(b/1024, 2)])
-  const body = `counter: ${counter++}. boot: ${boot}. Memory: ${memory}`
+  const body = `counter: ${counter++}. boot: ${boot}. Memory: ${memory}. url: ${req.url}. Version: ${VERSION}`
   
   return new Response(body, {
     headers: { "content-type": "text/plain" },
